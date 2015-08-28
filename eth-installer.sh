eabcdbae4223932704e0ae5f04c607a46427488f
phonikg <jgerryts@gmail.com>
2015-08-28 15:56:51 -0400

    Update eth-installer.sh

--- eth-installer.sh
+++ eth-installer.sh
@@ -3,9 +3,17 @@
 #First we need to perform some housekeeping & install dependencies
 sudo apt-get -y update
 sudo apt-get -y upgrade
-sudo apt-get -y install nano screen dphys-swapfile build-essential git python curl scons cmake nodejs npm ntp cloud-utils golang libboost-all-dev automake unzip libgmp-dev libgmp3-dev libtool libleveldb-dev yasm libminiupnpc-dev libreadline-dev libncurses5-dev libcurl4-openssl-dev wget libjsoncpp-dev libargtable2-dev clang-3.5 lldb-3.5 libmicrohttpd-dev clang-3.5-doc clang-3.5-examples clang-format-3.5 clang-modernize-3.5 cpp11-migrate-3.5 libclang-3.5-dev libclang-common-3.5-dev libclang1-3.5 libclang1-3.5-dbg liblldb-3.5 liblldb-3.5-dev libllvm-3.5-ocaml-dev libllvm3.5 libllvm3.5-dbg lldb-3.5-dev llvm-3.5 llvm-3.5-dev llvm-3.5-doc llvm-3.5-examples llvm-3.5-runtime llvm-3.5-tools python-clang-3.5 python-lldb-3.5
+sudo apt-get -y install raspi-copies-and-fills build-essential git python curl scons cmake libboost-all-dev automake unzip libgmp-dev libgmp3-dev libtool libleveldb-dev yasm libminiupnpc-dev libreadline-dev libncurses5-dev libcurl4-openssl-dev wget libjsoncpp-dev libargtable2-dev libmicrohttpd-dev libedit-dev mesa-common-dev ocl-icd-libopencl1 opencl-headers libgoogle-perftools-dev ocl-icd-dev libv8-dev binfmt-support libffi-dev libobjc-4.9-dev libobjc4
 
-#We must compile cryptopp & json-rpc-cpp from source "the old way" for armhf
+#Download armhf pre-compiled llvm-3.8 
+cd ~
+wget http://www.ethembedded.com/downloads/llvm-38-deb-files/llvm_deb_files.zip
+unzip llvm_deb_files.zip
+cd llvm_deb_files
+sudo dpkg -i *.deb
+cd ..
+
+#We must compile cryptopp & json-rpc-cpp from source for armhf
 
 #Cryptopp
 git clone https://github.com/mmoss/cryptopp.git
@@ -24,13 +32,13 @@ sudo ldconfig
 cd ../..
 
 #Now lets install Ethereum 
-
 cd ~
+git clone https://github.com/ethereum/cpp-ethereum-cmake
 git clone https://github.com/ethereum/cpp-ethereum
 cd cpp-ethereum
-git checkout develop
+git checkout master
 mkdir build
 cd build
-sudo cmake .. -DCMAKE_BUILD_TYPE=Release -DBUNDLE=minimal
-sudo make -j2
+cmake .. -DCMAKE_BUILD_TYPE=Release -DBUNDLE=minimal -DEVMJIT=off
+make -j2
 cd ../..
